17176c28d5360479f615d450421815f269ceda24
Eleazar <jai690@hotmail.com>
2022-01-20 14:16:47 -0600

    agregar perrito clip

--- src/Components/MainFrame.js
+++ src/Components/MainFrame.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
+import '../Styles/MainFrame.css';
 
 const MainFrame = props => {
     const [variable,setVariable] = useState();
     
     return (
-        <div>
-            
+        <div className='Principal'>
+            <img className='clip' src='clips/perrito.gif'/>
         </div>
     );
      
