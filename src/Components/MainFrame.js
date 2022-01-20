a5e0f5d520cbf301f37764bba3f7af77dc2a9c93
Eleazar <jai690@hotmail.com>
2022-01-20 14:03:22 -0600

    Create MainFrame

---
+++ src/Components/MainFrame.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+const MainFrame = props => {
+    const [variable,setVariable] = useState();
+    
+    return (
+        <div>
+            
+        </div>
+    );
+     
+};
+ 
+export default MainFrame;
