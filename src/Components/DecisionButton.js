ec1a961fb19db7c19e65a0f08b50c5cec1dbc8df
Eleazar <jai690@hotmail.com>
2022-01-21 11:18:56 -0600

    giving options to buttons base on state

--- src/Components/DecisionButton.js
+++ src/Components/DecisionButton.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import '../Styles/DecisionButton.css'
+
+const decisions = {
+    'A': 'This is state A',
+    'A1': 'This is state A1',
+    'A12': 'This is state A11',
+    'A12': 'This is state A12',
+    'A2': 'This is state A2',
+    'A21': 'This is state A21',
+    'A22': 'This is state A22',
+    'B': 'This is state B',
+    'B1': 'This is state B1',
+    'B12': 'This is state B11',
+    'B12': 'This is state B12',
+    'B2': 'This is state B2',
+    'B21': 'This is state B21',
+    'B22': 'This is state B22',
+}
+
 
 const DecisionButton = props => {
 
 
     return (
         <div>
-            <button onClick={() => props.onClickHandler(props.decisionA)}>{props.decisionA}</button>
-            <button onClick={() => props.onClickHandler(props.decisionB)}>{props.decisionB}</button>
+            <button className='button' onClick={() => props.onClickHandler(props.decisionA)}>{decisions[props.decisionA]}</button>
+            <button className='button' onClick={() => props.onClickHandler(props.decisionB)}>{decisions[props.decisionB]}</button>
         </div>
     );
      
