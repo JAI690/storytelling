ae1184e1d7bd93e2333bc1cc7a8a567ed36934b8
Eleazar <jai690@hotmail.com>
2022-01-20 21:54:16 -0600

    Estilo button start

--- src/Components/StartButton.js
+++ src/Components/StartButton.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
+import '../Styles/StartButton.css'
 
 const StartButton = props => {
     const [variable,setVariable] = useState();
     
     return (
         <div>
-            
+            <button className='startButton' >START</button>
         </div>
     );
      
