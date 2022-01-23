3da0b72033d69562793b2bbf90b1651915a0d41f
Eleazar <jai690@hotmail.com>
2022-01-23 14:10:10 -0600

    initialState

--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -1,12 +1,12 @@
 import * as actions from './actionTypes';
 
-export default function reducer(state,action){
+export default function reducer(state = {history: 'start'},action){
     switch (action.type) {
         case actions.TAKE_DECISION:
             return {
                 ...state,
-                history: action.paylad.history,
-            }
+                history: action.payload.decision,
+            };
     
         default:
             return state
