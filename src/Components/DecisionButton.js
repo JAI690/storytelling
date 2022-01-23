3e7372d6e318c46cc905815d5041bd22d090645b
Eleazar <jai690@hotmail.com>
2022-01-23 13:44:50 -0600

    Refactoring

--- src/Components/DecisionButton.js
+++ src/Components/DecisionButton.js
@@ -1,23 +1,7 @@
 import React from 'react';
 import '../Styles/DecisionButton.css'
 
-const decisions = {
-    'A': 'Work',
-    'A1': 'Work remote',
-    'A11': 'Start Working',
-    'A12': 'Simulate Working',
-    'A2': 'Work on-site',
-    'A21': 'Sleep at work',
-    'A22': 'Ask for UTO',
-    'B': 'Day off',
-    'B1': 'Excercising',
-    'B11': 'Take pills *',
-    'B12': 'Go home',
-    'B2': 'Eating',
-    'B21': 'Am I eating too much?',
-    'B22': 'Don\'t care...',
-}
-
+import { decisions } from '../Dictionary';
 
 const DecisionButton = props => {
 
--- src/Components/MainFrame.js
+++ src/Components/MainFrame.js
@@ -2,17 +2,16 @@ import React, { useState } from 'react';
 import '../Styles/MainFrame.css';
 import Message from './Message';
 import ClipDisplay from './ClipDisplay';
+import { clips } from '../Dictionary';
 
 const MainFrame = props => {
     const [messageState,setMessageState] = useState(false);
-    const [clip,setClip] = useState('intro');
     const [history, setHistory] = useState('start')
     const [loading, setLoading] = useState(false);
 
     const restart = function(){
         setMessageState(false);
         setHistory('start')
-        setClip('intro');
     }
 
     const changeState = function(){
@@ -20,90 +19,27 @@ const MainFrame = props => {
     };
 
     const transition = function(){
-        setClip('waiting');
+            setHistory('waiting');
     }
 
-    const selectHistory = (decision)=>{
-        transition();
-        setHistory(decision);
+    const selectHistory = async(decision)=>{
         setLoading(true)
-        if(decision==='start'){setHistory('')}
+        transition();
         setTimeout(() => {
-            changeClip(decision);
+            if(decision==='start'){
+                setHistory('')
+            }else{
+                setHistory(decision);
+            }
             setLoading(false);
         }, 1000);
-
-    };
-
-    const changeClip = function(decision){
-
-        switch (decision) {
-            case 'start':
-                setClip('go-sleep');
-                break;
-
-            case 'A':
-                setClip('wakingup');
-                break;
-
-                case 'A1':
-                    setClip('sitting to work');
-                    break;
-
-                    case 'A11':
-                        setClip('typing');
-                        break;
-                    case 'A12':
-                        setClip('working-remote');
-                        break;
-
-                case 'A2':
-                    setClip('spin-chair');
-                    break;
-
-                    case 'A21':
-                        setClip('work-onfire');
-                        break;
-                    case 'A22':
-                        setClip('work-leaving');
-                        break;
-
-            case 'B':
-                setClip('wakingup');
-                break;
-
-                case 'B1':
-                    setClip('excercesing');
-                    break;
-                    
-                    case 'B11':
-                        setClip('jogging');
-                        break;
-                    case 'B12':
-                        setClip('wakingup2');
-                        break;
-
-                case 'B2':
-                    setClip('eating');
-                    break;    
-                    
-                    case 'B21':
-                        setClip('thinking');
-                        break;   
-                    case 'B22':
-                        setClip('tiburon');
-                        break;   
-            default:
-                setClip('perrito')
-                break;
-        }
     };
 
 
     return (
         <div className='Principal' onClick={() => {changeState()}} >
 
-            <ClipDisplay state={history} src= {'clips/'+clip+'.gif'}  />
+            <ClipDisplay state={history} src= {'clips/'+clips[history]+'.gif'}  />
 
             <div style={messageState ?  {display: ''} : {display: 'none'}}>
 
